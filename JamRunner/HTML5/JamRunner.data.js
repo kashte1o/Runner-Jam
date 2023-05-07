8854a5b64435d977443f23f6d7e27ebce149ff55
KrivoshapkinDaniil <106012164+KrivoshapkinDaniil@users.noreply.github.com>
2023-05-07 18:52:53 +0300

    jvjhg

--- JamRunner/HTML5/JamRunner.data.js
+++ JamRunner/HTML5/JamRunner.data.js
@@ -173,6 +173,6 @@ Module['FS_createPath']('/JamRunner/Content', 'Paks', true, true);
   }
 
  }
- loadPackage({"files": [{"start": 0, "audio": 0, "end": 53, "filename": "/Manifest_NonUFSFiles_HTML5.txt"}, {"start": 53, "audio": 0, "end": 91, "filename": "/UE4CommandLine.txt"}, {"start": 91, "audio": 0, "end": 89997026, "filename": "/JamRunner/Content/Paks/JamRunner-HTML5.pak"}], "remote_package_size": 89997026, "package_uuid": "e122f025-4c21-4bda-a24d-460be39421b6"});
+ loadPackage({"files": [{"start": 0, "audio": 0, "end": 53, "filename": "/Manifest_NonUFSFiles_HTML5.txt"}, {"start": 53, "audio": 0, "end": 91, "filename": "/UE4CommandLine.txt"}, {"start": 91, "audio": 0, "end": 89374200, "filename": "/JamRunner/Content/Paks/JamRunner-HTML5.pak"}], "remote_package_size": 89374200, "package_uuid": "c73f400f-218f-4365-9352-871b05c128a4"});
 
 })();
